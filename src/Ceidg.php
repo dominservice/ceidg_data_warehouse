1947795f3c1cd159ac52f5f523e8e7797f45996d
dso-it <biuro@dso.biz.pl>
2022-07-14 10:03:00 +0200

    init

--- src/Ceidg.php
+++ src/Ceidg.php
@@ -130,6 +130,7 @@ class Ceidg
         }
 
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, 20);
